c8879174e3093e418cab344ff260dfc17b043cc8
ybmuidbanten <mirpan944@gmail.com>
2022-11-20 19:44:23 +0700

    uts apicovid muhamad irpan

---
+++ Documents/UTS_APICovid/database/migrations/2022_11_19_125016_uts_apicovid.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UtsApicovid extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        #membuat tabel patients 
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->text('address');
+            $table->string('status');
+            $table->date('in_date_at');
+            $table->date('out_date_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
